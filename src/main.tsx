9e5e58705e14f4142fef2f703bdaf87d3eccfe2c
ali <ali.raed31@gmail.com>
2026-09-22 01:09:10 +0100

    fix index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import App from "./App";
 import "./index.css";
-import App from "./App.tsx";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const convexUrl = import.meta.env.VITE_CONVEX_URL;
+
+if (!convexUrl) {
+  throw new Error("VITE_CONVEX_URL is missing");
+}
+
+const convex = new ConvexReactClient(convexUrl);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <ConvexProvider client={convex}>
       <App />
     </ConvexProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 );
